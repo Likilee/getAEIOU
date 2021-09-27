78b277d15a42d0a1f9c6820b40399169104262cd
Likilee <gnsdlrl@daum.net>
2021-09-27 16:57:13 +0900

    Init

---
+++ test/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import Box from './component/box';
+
+function App(): JSX.Element {
+  return <Box />;
+}
+
+export default App;
---
+++ test/src/component/box.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './box.css';
+
+function box(): JSX.Element {
+  return (
+    <div id="box_wrapper">
+      <div id="box_title">Title</div>
+      <div id="box_content">Content</div>
+    </div>
+  );
+}
+
+export default box;
