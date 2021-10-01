66485e961a473612888a60e961c48369cef83909
Likilee <gnsdlrl@daum.net>
2021-10-01 17:03:36 +0900

    Update

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -1,37 +1,9 @@
 import React from 'react';
 import './App.css';
-import Setting, {Formant} from './component/Setting';
-
-const formants: Formant[] = [
-  {
-    label: '아',
-    array: [],
-    Image: null,
-  },
-  {
-    label: '이',
-    array: [],
-    Image: null,
-  },
-  {
-    label: '우',
-    array: [],
-    Image: null,
-  },
-  {
-    label: '애',
-    array: [],
-    Image: null,
-  },
-  {
-    label: '오',
-    array: [],
-    Image: null,
-  },
-];
+import Setting from './component/Setting';
 
 function App(): JSX.Element {
-  return <Setting formants={formants} />;
+  return <Setting />;
 }
 
 export default App;
--- test/src/component/Setting.tsx
+++ test/src/component/Setting.tsx
@@ -1,140 +1,40 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useReducer, useRef, useState} from 'react';
+import './style.css';
+// interface SettingProps {
+//   // formants: Formant[];
+// }
 
-export interface Formant {
-  label: string;
-  array: number[];
-  Image: HTMLImageElement | null;
-}
-
-const formants: Formant[] = [
-  {
-    label: '아',
-    array: [],
-    Image: null,
-  },
-  {
-    label: '이',
-    array: [],
-    Image: null,
-  },
-  {
-    label: '우',
-    array: [],
-    Image: null,
-  },
-  {
-    label: '애',
-    array: [],
-    Image: null,
-  },
-  {
-    label: '오',
-    array: [],
-    Image: null,
-  },
-];
-
-interface VowelInputProps {
-  vowel: string;
-  setVowels: (arg0: number[]) => void;
-  smad: number[];
-}
-
-function VowelInput(props: VowelInputProps) {
-  const [isShow, setIsShow] = useState(false);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  const resetClick = () => {
-    props.setVowels([]);
-  };
-  const doneClick = () => {
-    console.log(props.smad);
-    props.setVowels([...props.smad]);
-  };
-  const showClick = () => {
-    setIsShow(!isShow);
-  };
-  useEffect(() => {
-    if (canvasRef.current && isShow) {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext('2d');
-      if (!ctx) return;
-      const animationCb = () => {
-        formants.forEach(value => {
-          if (value.label === props.vowel) {
-            value.array.forEach((val, idx) => {
-              ctx.beginPath();
-              ctx.moveTo(idx, canvas.clientHeight);
-              ctx.lineTo(idx, canvas.clientHeight - val);
-              ctx.stroke();
-            });
-          }
-        });
-        requestAnimationFrame(animationCb);
-      };
-      requestAnimationFrame(animationCb);
-    }
-  });
-
+function Title(): JSX.Element {
   return (
-    <>
-      {isShow ? (
-        <canvas
-          style={{border: '1px solid black'}}
-          width={1024}
-          height={190}
-          ref={canvasRef}
-        ></canvas>
-      ) : null}
-      <div>
-        {props.vowel}
-        <button onClick={resetClick}>reset</button>
-        <button onClick={doneClick}>done</button>
-        <button onClick={showClick}>show</button>
-      </div>
-    </>
+    <div className="Title">
+      <h3>Title</h3>
+    </div>
   );
 }
 
-interface SettingProps {
-  formants: Formant[];
+function Message(): JSX.Element {
+  return (
+    <div className="Message">
+      <span>Message</span>
+    </div>
+  );
 }
 
-function Setting(props: SettingProps): JSX.Element {
-  const smad: number[] = [];
-  useEffect(() => {
-    navigator.mediaDevices
-      .getUserMedia({audio: true, video: false})
-      .then(stream => {
-        const audioContext = new AudioContext();
-        const source = audioContext.createMediaStreamSource(stream);
-        const analyser = audioContext.createAnalyser();
-        source.connect(analyser);
-        console.log(audioContext.sampleRate);
-        analyser.smoothingTimeConstant = 0.6;
-        analyser.fftSize = 2048; //
-        const byteFrequencyDataArray = new Uint8Array(
-          analyser.frequencyBinCount / 3,
-        );
-      });
-  }, []);
+function ProgressBar(): JSX.Element {
+  return <div className="ProgressBar">progressbars</div>;
+}
 
+function VowelInputPanel(): JSX.Element {
+  return <div className="VowelInputPanel">1</div>;
+}
+function Setting(): JSX.Element {
   return (
-    <>
-      {formants.map((value, idx) => {
-        const setVowels = (arg0: number[]) => {
-          formants[idx].array = arg0;
-        };
-        return (
-          <VowelInput
-            key="tmp"
-            vowel={value.label}
-            setVowels={setVowels}
-            smad={smad}
-          ></VowelInput>
-        );
-      })}
-    </>
+    <div className="Setting">
+      <Title />
+      <Message />
+      <ProgressBar />
+      <VowelInputPanel />
+    </div>
   );
 }
 
